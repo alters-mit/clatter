e630cb2c467d5547be6277f08134df2c1036419a
Seth Alter <alters@mit.edu>
2022-12-14 15:25:06 -0500

    Fixed Creator

--- Clatter/Clatter.Core/Creator.cs
+++ Clatter/Clatter.Core/Creator.cs
@@ -8,18 +8,37 @@ namespace Clatter.Core
     /// </summary>
     public static class Creator
     {
-        public static byte[] GetImpact(byte primaryImpactMaterial, float primaryAmp, float primaryResonance, float primaryMass, 
-            byte secondaryImpactMaterial, float secondaryAmp, float secondaryResonance, float secondaryMass, float speed)
+        /// <summary>
+        /// The primary object.
+        /// </summary>
+        private static AudioObjectData primary;
+        /// <summary>
+        /// The secondary object.
+        /// </summary>
+        private static AudioObjectData secondary;
+        /// <summary>
+        /// A cached impact. This can be used for ongoing impacts.
+        /// </summary>
+        private static Impact impact;
+
+
+        /// <summary>
+        /// Generate an impact sound.
+        /// This assumes that you have called SetPrimaryObject and SetSecondaryObject.
+        /// </summary>
+        /// <param name="speed">The speed of the impact.</param>
+        /// <param name="newImpact">If true, this is a new impact. If false, this is part of an ongoing series of impacts. Ignored the first time this function is called.</param>
+        public static byte[] GetImpact(float speed, bool newImpact)
         {
-            // Create the objects.
-            AudioObjectData a = new AudioObjectData(0, (ImpactMaterialSized)primaryImpactMaterial, primaryAmp, primaryResonance, primaryMass);
-            AudioObjectData b = new AudioObjectData(0, (ImpactMaterialSized)secondaryImpactMaterial, secondaryAmp, secondaryResonance, secondaryMass);
             // Create the random number generator.
             Random rng = new Random();
-            // Get the impact.
-            Impact impact = new Impact(a, b, rng);
+            // Set the impact.
+            if (newImpact || impact == null)
+            {
+                impact = new Impact(primary, secondary, rng);
+            }
             // Get the collision event.
-            CollisionEvent collisionEvent = new CollisionEvent(a, b, 0, speed, 0, Vector3d.Zero, OnCollisionType.enter, false);
+            CollisionEvent collisionEvent = new CollisionEvent(primary, secondary, 0, speed, 0, Vector3d.Zero, OnCollisionType.enter, false);
             // Generate audio.
             bool ok = impact.GetAudio(collisionEvent, rng);
             if (!ok)
@@ -31,25 +50,27 @@ namespace Clatter.Core
                 return impact.samples.ToInt16Bytes();
             }
         }
-
-
-        public static byte[] GetScrape(byte primaryImpactMaterial, float primaryAmp, float primaryResonance, float primaryMass,
-            byte secondaryImpactMaterial, float secondaryAmp, float secondaryResonance, float secondaryMass, byte scrapeMaterial, float speed, int count)
+        
+        /// <summary>
+        /// Generate a scrape sound.
+        /// This assumes that you have called SetPrimaryObject and SetSecondaryObject, and that in the latter call you provided a scrape material.
+        /// </summary>
+        /// <param name="speed">The speed of the scrape.</param>
+        /// <param name="duration">The duration of the scrape in seconds. This will be rounded to the nearest tenth of a second.</param>
+        public static byte[] GetScrape(float speed, float duration)
         {
+            int count = (int)(duration * Globals.framerate / Scrape.SAMPLES_LENGTH);
             // Get the scrape material.
-            ScrapeMaterial sm = (ScrapeMaterial)scrapeMaterial;
             // Create the objects.
-            AudioObjectData a = new AudioObjectData(0, (ImpactMaterialSized)primaryImpactMaterial, primaryAmp, primaryResonance, primaryMass);
-            AudioObjectData b = new AudioObjectData(0, (ImpactMaterialSized)secondaryImpactMaterial, secondaryAmp, secondaryResonance, secondaryMass, sm);
-            a.hasPreviousArea = true;
-            a.previousArea = 1;
-            a.speed = speed;
+            primary.hasPreviousArea = true;
+            primary.previousArea = 1;
+            primary.speed = speed;
             // Create the random number generator.
             Random rng = new Random();
             // Get the scrape.
-            Scrape scrape = new Scrape(sm, a, b, rng);
+            Scrape scrape = new Scrape(secondary.scrapeMaterial, primary, secondary, rng);
             // Get the collision event.
-            CollisionEvent collisionEvent = new CollisionEvent(a, b, 0, speed, 1, Vector3d.Zero, OnCollisionType.stay, false);
+            CollisionEvent collisionEvent = new CollisionEvent(primary, secondary, 0, speed, 1, Vector3d.Zero, OnCollisionType.stay, false);
             byte[] audio = new byte[Scrape.SAMPLES_LENGTH * 2 * count];
             for (int i = 0; i < count; i++)
             {
@@ -59,6 +80,34 @@ namespace Clatter.Core
                 Buffer.BlockCopy(scrape.samples.ToInt16Bytes(), 0, audio, i * 2 * count, Scrape.SAMPLES_LENGTH * 2);
             }
             return audio;
+        }
+
+        
+        /// <summary>
+        /// Set the primary object.
+        /// </summary>
+        /// <param name="impactMaterial">A byte representing the impact material.</param>
+        /// <param name="amp">The amp.</param>
+        /// <param name="resonance">The resonance.</param>
+        /// <param name="mass">The mass.</param>
+        public static void SetPrimaryObject(byte impactMaterial, float amp, float resonance, float mass)
+        {
+            primary = new AudioObjectData(0, (ImpactMaterialSized)impactMaterial, amp, resonance, mass);
+        }
+
+
+        /// <summary>
+        /// Set the secondary object.
+        /// </summary>
+        /// <param name="impactMaterial">A byte representing the impact material.</param>
+        /// <param name="amp">The amp.</param>
+        /// <param name="resonance">The resonance.</param>
+        /// <param name="mass">The mass.</param>
+        /// <param name="scrapeMaterial">The scrape material. Can be null.</param>
+        public static void SetSecondaryObject(byte impactMaterial, float amp, float resonance, float mass, byte? scrapeMaterial)
+        {
+            secondary = new AudioObjectData(1, (ImpactMaterialSized)impactMaterial, amp, resonance, mass,
+                (ScrapeMaterial?)scrapeMaterial);
         }
     }
 }
