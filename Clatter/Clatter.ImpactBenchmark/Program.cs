d78ab8affbaf18d9cb68e61eb15a496ad1f3c8f0
Seth Alter <alters@mit.edu>
2022-12-19 11:20:40 -0500

    A fair test?

--- Clatter/Clatter.ImpactBenchmark/Program.cs
+++ Clatter/Clatter.ImpactBenchmark/Program.cs
@@ -8,13 +8,15 @@ namespace Clatter.ImpactBenchmark
     {
         private static void Main(string[] args)
         {
+            Creator.SetPrimaryObject(new AudioObjectData(0, ImpactMaterialSized.glass_1, 0.2f, 0.2f, 1));
+            Creator.SetSecondaryObject(new AudioObjectData(1, ImpactMaterialSized.stone_4, 0.5f, 0.1f, 100));
+            Impact.maxTimeBetweenImpacts = 1000;
+            Impact.minTimeBetweenImpacts = 0.0001;
             Stopwatch watch = new Stopwatch();
             watch.Start();
             for (int i = 0; i < 100; i++)
             {
-                Creator.SetPrimaryObject(new AudioObjectData(0, ImpactMaterialSized.glass_1, 0.2f, 0.2f, 1));
-                Creator.SetSecondaryObject(new AudioObjectData(1, ImpactMaterialSized.stone_4, 0.5f, 0.1f, 100));
-                Creator.GetImpact(1, true);       
+                Creator.GetImpact(1, i == 0);       
             }
             watch.Stop();
             Console.WriteLine("Total time: " + watch.Elapsed.TotalSeconds);
