abcdf401f4170ef69322fa39f6f17eedbbd97ce6
Seth Alter <alters@mit.edu>
2022-12-20 12:30:30 -0500

    benchmark

---
+++ Clatter/Clatter.ScrapeBenchmark/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics;
+using Clatter.Core;
+
+
+namespace Clatter.ScrapeBenchmark
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Creator.SetPrimaryObject(new AudioObjectData(0, ImpactMaterialSized.metal_1, 0.2f, 0.2f, 1));
+            Creator.SetSecondaryObject(new AudioObjectData(1, ImpactMaterialSized.stone_4, 0.5f, 0.1f, 100, ScrapeMaterial.ceramic));
+            Creator.SetRandom(0);
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            Creator.GetScrape(1f, 10f);
+            watch.Stop();
+            Console.WriteLine(watch.Elapsed.TotalSeconds);
+        }
+    }
+}
