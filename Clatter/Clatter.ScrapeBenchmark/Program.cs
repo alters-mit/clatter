3b530ada89db7933489f2a633bd069eda3a557a0
Seth Alter <alters@mit.edu>
2022-12-21 08:33:36 -0500

    minor

--- Clatter/Clatter.ScrapeBenchmark/Program.cs
+++ Clatter/Clatter.ScrapeBenchmark/Program.cs
@@ -8,8 +8,17 @@ namespace Clatter.ScrapeBenchmark
     {
         private static void Main(string[] args)
         {
-            Creator.SetPrimaryObject(new AudioObjectData(0, ImpactMaterialSized.metal_1, 0.2f, 0.2f, 1));
-            Creator.SetSecondaryObject(new AudioObjectData(1, ImpactMaterialSized.stone_4, 0.5f, 0.1f, 100, ScrapeMaterial.ceramic));
+            // Load the materials.
+            ScrapeMaterial scrapeMaterial = ScrapeMaterial.ceramic;
+            ScrapeMaterialData.Load(scrapeMaterial);
+            ImpactMaterialSized primaryImpactMaterial = ImpactMaterialSized.metal_1;
+            ImpactMaterialSized secondaryImpactMaterial = ImpactMaterialSized.stone_4;
+            ImpactMaterialData.Load(primaryImpactMaterial);
+            ImpactMaterialData.Load(secondaryImpactMaterial);
+            // Set the objects.
+            Creator.SetPrimaryObject(new AudioObjectData(0, primaryImpactMaterial, 0.2f, 0.2f, 1));
+            Creator.SetSecondaryObject(new AudioObjectData(1, secondaryImpactMaterial, 0.5f, 0.1f, 100, scrapeMaterial));
+            // Generate audio.
             Creator.SetRandom(0);
             Stopwatch watch = new Stopwatch();
             watch.Start();
